0c341fdfb3217d6f2938f59695b81f708624d350
jhedgeman12345 <jhedgeman12345@gmail.com>
2024-07-21 21:23:30 -0500

    Update COEMS.lua

--- COEMS.lua
+++ COEMS.lua
@@ -1,6 +1,6 @@
 -- Putting together everything we've learned, we will now create a basic hub using all previous code snippets.
 
-local Progress = getfenv().Progress
+local Progress = getgenv().Progress
 
 Progress.Window:SetSubtitle("BETA TEST | Early Access")
 
